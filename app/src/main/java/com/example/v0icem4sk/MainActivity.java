a5e95442ae1bc93e44857b7a392e6e95c68b9024
Tyler Johnson <tjohnson@saltstack.com>
2020-01-07 23:30:10 -0700

    Initial commit, speech-to-text is working
    Signed-off-by: Tyler Johnson <tjohnson@saltstack.com>

---
+++ app/src/main/java/com/example/v0icem4sk/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.v0icem4sk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private ImageButton mSpeakBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVoiceInputTv = findViewById(R.id.voiceInput);
+        mSpeakBtn = findViewById(R.id.btnSpeak);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
+            }
+        });
+    }
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mVoiceInputTv.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+}
+
+
