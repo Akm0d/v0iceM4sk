fcfde4cd9afe6e2a1c8308cd0ca0b91e074defc9
Tyler Johnson <tjohnson@saltstack.com>
2020-01-08 00:51:36 -0700

    Added text to speech basics
    Signed-off-by: Tyler Johnson <tjohnson@saltstack.com>

--- app/src/main/java/com/example/v0icem4sk/MainActivity.java
+++ app/src/main/java/com/example/v0icem4sk/MainActivity.java
@@ -13,7 +13,9 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +26,7 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements RecognitionListener {
+    // Speech Recorder variables
     private static final int REQUEST_RECORD_PERMISSION = 100;
     private TextView returnedText;
     private ToggleButton toggleButton;
@@ -32,11 +35,31 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
     private Intent recognizerIntent;
     private String LOG_TAG = "VoiceRecognitionActivity";
 
+    // Text Speaker variables
+    private Button speakNowButton;
+    private EditText editText;
+    TTSManager ttsManager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        // Initialize Speaker
+        ttsManager = new TTSManager();
+        ttsManager.init(this);
+
+        editText = (EditText) findViewById(R.id.input_text);
+        speakNowButton = (Button) findViewById(R.id.speak_now);
+        speakNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String text = editText.getText().toString();
+                ttsManager.initQueue(text);
+            }
+        });
+
+        // Initialize Recorder
         returnedText = findViewById(R.id.voiceInput);
         progressBar = findViewById(R.id.rms_fluctuations);
         toggleButton = findViewById(R.id.toggleSpeak);
@@ -69,6 +92,15 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
                 }
             }
         });
+    }
+
+    /**
+     * Releases the resources used by the TextToSpeech engine.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ttsManager.shutDown();
     }
 
     @Override
